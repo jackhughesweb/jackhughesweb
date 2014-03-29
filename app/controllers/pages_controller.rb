cceab4db33e1f901331e799c1a317e9d465f3fd1
jackhughesweb <jack@jackhughesweb.com>
2014-03-29 16:12:45 +0000

    Added index page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,8 @@
 class PagesController < ApplicationController
 
   def index
-    
+    @posts = Post.where(published: true).order("publish_date DESC").limit(3)
+    @projects = Project.where(published: true).order("publish_date DESC").limit(3)
   end
 
 end
